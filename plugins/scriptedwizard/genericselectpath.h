c47a4284bedc5d87a84760fbbd81d4834590b0bc
YWX <wxFortranIndent@163.com>
2014-05-26 21:05:31 +0800

    Code::Blocks Scripted Wizard Plugin alone Project init
    Code::Blocks 13.12 rev 9501 SDK Version 1.19.0
    Build: Dec 27 2013,12:58:38 - wx2.8.12
    Release 13.12 rev 9501 (2013/12/25 19:25:45) gcc 4.7.1 Windows/unicode - 32 bit

---
+++ plugins/scriptedwizard/genericselectpath.h
@@ -0,0 +1,63 @@
+/*
+ * This file is part of the Code::Blocks IDE and licensed under the GNU General Public License, version 3
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+#ifndef GENERICSELECTPATH_H
+#define GENERICSELECTPATH_H
+
+#ifdef __BORLANDC__
+    #pragma hdrstop
+#endif
+
+//(*Headers(GenericSelectPath)
+#include <wx/sizer.h>
+#include <wx/stattext.h>
+#include <wx/textctrl.h>
+#include <wx/panel.h>
+#include <wx/button.h>
+//*)
+
+class wxString;
+
+class GenericSelectPath: public wxPanel
+{
+	public:
+
+		GenericSelectPath(wxWindow* parent,wxWindowID id = -1);
+		virtual ~GenericSelectPath();
+
+		// use this because it adjusts the sizer too
+		void SetDescription(const wxString& descr)
+		{
+		    lblDescr->SetLabel(descr);
+
+            GetSizer()->Fit(this);
+            GetSizer()->SetSizeHints(this);
+		}
+
+		//(*Identifiers(GenericSelectPath)
+		static const long ID_STATICTEXT1;
+		static const long ID_STATICTEXT2;
+		static const long ID_TEXTCTRL1;
+		static const long ID_BUTTON1;
+		//*)
+
+		//(*Handlers(GenericSelectPath)
+		//*)
+
+		//(*Declarations(GenericSelectPath)
+		wxButton* btnBrowse;
+		wxStaticText* lblDescr;
+		wxBoxSizer* BoxSizer2;
+		wxTextCtrl* txtFolder;
+		wxBoxSizer* BoxSizer1;
+		wxStaticText* lblLabel;
+		//*)
+
+	private:
+
+		DECLARE_EVENT_TABLE()
+};
+
+#endif
---
+++ plugins/scriptedwizard/genericsinglechoicelist.cpp
@@ -0,0 +1,60 @@
+/*
+ * This file is part of the Code::Blocks IDE and licensed under the GNU General Public License, version 3
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * $Revision: 7109 $
+ * $Id: genericsinglechoicelist.cpp 7109 2011-04-15 11:53:16Z mortenmacfly $
+ * $HeadURL: http://svn.code.sf.net/p/codeblocks/code/branches/release-xx.yy/src/plugins/scriptedwizard/genericsinglechoicelist.cpp $
+ */
+
+#include "genericsinglechoicelist.h"
+
+#include <wx/arrstr.h>
+#include <wx/intl.h>
+#include <wx/string.h>
+
+//(*IdInit(GenericSingleChoiceList)
+const long GenericSingleChoiceList::ID_STATICTEXT1 = wxNewId();
+const long GenericSingleChoiceList::ID_LISTBOX1 = wxNewId();
+//*)
+
+BEGIN_EVENT_TABLE(GenericSingleChoiceList,wxPanel)
+	//(*EventTable(GenericSingleChoiceList)
+	//*)
+END_EVENT_TABLE()
+
+GenericSingleChoiceList::GenericSingleChoiceList(wxWindow* parent,wxWindowID id)
+{
+	//(*Initialize(GenericSingleChoiceList)
+	wxBoxSizer* BoxSizer1;
+	wxStaticBoxSizer* StaticBoxSizer1;
+
+	Create(parent, id, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL, _T("id"));
+	BoxSizer1 = new wxBoxSizer(wxVERTICAL);
+	lblDescr = new wxStaticText(this, ID_STATICTEXT1, _("Description"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT1"));
+	BoxSizer1->Add(lblDescr, 0, wxALL|wxEXPAND|wxALIGN_LEFT|wxALIGN_TOP, 8);
+	StaticBoxSizer1 = new wxStaticBoxSizer(wxVERTICAL, this, _("Please make a selection"));
+	GenericChoiceList = new wxListBox(this, ID_LISTBOX1, wxDefaultPosition, wxSize(232,131), 0, 0, wxLB_SINGLE|wxLB_HSCROLL, wxDefaultValidator, _T("ID_LISTBOX1"));
+	StaticBoxSizer1->Add(GenericChoiceList, 1, wxALL|wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 4);
+	BoxSizer1->Add(StaticBoxSizer1, 1, wxALL|wxEXPAND|wxFIXED_MINSIZE|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 8);
+	SetSizer(BoxSizer1);
+	BoxSizer1->Fit(this);
+	BoxSizer1->SetSizeHints(this);
+	//*)
+
+	GenericChoiceList->SetName(_T("GenericChoiceList"));
+}
+
+GenericSingleChoiceList::~GenericSingleChoiceList()
+{
+}
+
+void GenericSingleChoiceList::SetChoices(const wxArrayString& choices, int defChoice)
+{
+    GenericChoiceList->Clear();
+    for (size_t i = 0; i < choices.GetCount(); ++i)
+    {
+        GenericChoiceList->Append(choices[i]);
+    }
+    GenericChoiceList->SetSelection(defChoice);
+}
